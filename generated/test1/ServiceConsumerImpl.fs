88e81a2e727e7030d81a9a547de76cd4052fdf25
Eduardo Burgos <eburgos@gmail.com>
2014-12-06 11:03:13 -0400

    AMQP-Rpc over F# mostly working

---
+++ generated/test1/ServiceConsumerImpl.fs
@@ -0,0 +1,53 @@
+﻿namespace NSchema.Model.Invoicing
+
+/// <summary></summary>
+type InvoiceServiceSrv(connectionString: string, queueName: string) =
+    inherit InvoiceServiceConsumer(connectionString, queueName)
+    override this.Authenticate userName password =
+        match userName, password with
+        | "manager", "password" ->
+            new UserInfo(Name = "manager", Status = NSchema.Model.Invoicing.AuthenticationStatus.LoggedIn)
+        | usr, _ ->
+            new UserInfo(Name = usr, Status = NSchema.Model.Invoicing.AuthenticationStatus.LoggedOut)
+    override this.GetInvoiceList userName password =
+        match (this.Authenticate userName password).Status with
+        | NSchema.Model.Invoicing.AuthenticationStatus.LoggedIn ->
+            [
+                new Invoice(
+                    CustomerId = 1234,
+                    Details = [
+                        new InvoiceDetail(ProductId=11, Description="Diet 7up", Quantity=4.0, UnitPrice=1.25);
+                        new InvoiceDetail(ProductId=12, Description="Diet Pepsi", Quantity=3.0, UnitPrice=1.20)
+                    ]);
+                new Invoice(
+                    CustomerId = 1235, 
+                    Details = [
+                        new InvoiceDetail(ProductId=21, Description="Croissant", Quantity=1.0, UnitPrice=3.25);
+                        new InvoiceDetail(ProductId=22, Description="CheeseBurger", Quantity=1.0, UnitPrice=5.25)
+                    ]
+                )
+            ]
+        | _ ->
+            []
+    override this.GetTwoValueOperation userName password =
+        match (this.Authenticate userName password).Status with
+        | NSchema.Model.Invoicing.AuthenticationStatus.LoggedIn ->
+            ([
+                new Invoice(
+                    CustomerId = 1234,
+                    Details = [
+                        new InvoiceDetail(ProductId=11, Description="Diet 7up", Quantity=4.0, UnitPrice=1.25);
+                        new InvoiceDetail(ProductId=12, Description="Diet Pepsi", Quantity=3.0, UnitPrice=1.20)
+                    ]);
+                new Invoice(
+                    CustomerId = 1235, 
+                    Details = [
+                        new InvoiceDetail(ProductId=21, Description="Croissant", Quantity=1.0, UnitPrice=3.25);
+                        new InvoiceDetail(ProductId=22, Description="CheeseBurger", Quantity=1.0, UnitPrice=5.25)
+                    ]
+                )
+            ], null)
+        | _ ->
+            ([], "Unauthorized")
+    
+    
--- generated/test1/Test.fs
+++ generated/test1/Test.fs
@@ -5,43 +5,26 @@ open Newtonsoft.Json
 open Newtonsoft.Json.Converters
 
 [<EntryPoint>]
-let main args = 
-    let stroriginal = "[1, { edad: 12 \r\n}, \"casa\"\n\r,\nfalse\n, \n{nombre:\n \"Amanda\"},\r undefined,\n 2.5446, [1\r\n, { edad: 12 }, \"casa\"\n,false, {nombre: \"Amanda\"}, undefined, 2]]"
-    let str = stroriginal.Replace("\r\n", "").Replace("\n", "").Replace("\r", "")
-    
-    use textReader = new System.IO.StringReader (str)
-    let reader = new JsonTextReader (textReader)
-
-    let rec tokenSplit (reader: JsonTextReader) acc startLine startIdx: string list =
-        reader.Read() |> ignore
-        match reader.Depth, reader.TokenType with
-        | _, JsonToken.None ->
-            acc
-        | 0, JsonToken.StartArray ->
-            tokenSplit reader acc reader.LineNumber reader.LinePosition
-        | 1, JsonToken.StartObject | 1, JsonToken.StartArray ->
-            tokenSplit reader acc reader.LineNumber (reader.LinePosition - 1)
-        | 1, JsonToken.EndObject | 1, JsonToken.EndArray ->
-            let newValue = str.Substring(startIdx, reader.LinePosition - startIdx)
-            tokenSplit reader (newValue::acc) reader.LineNumber (reader.LinePosition + 1)
-        | 1, JsonToken.String ->
-            let newValue = "\"" + (reader.Value |> string) + "\""
-            tokenSplit reader (newValue::acc) reader.LineNumber (reader.LinePosition + 1)
-        | 1, JsonToken.Boolean ->
-            let newValue = (reader.Value |> string).ToLower()
-            tokenSplit reader (newValue::acc) reader.LineNumber (reader.LinePosition + 1)
-        | 1, JsonToken.Undefined ->
-            let newValue = "undefined"
-            tokenSplit reader (newValue::acc) reader.LineNumber (reader.LinePosition + 1)
-        | 1, JsonToken.Null ->
-            let newValue = "null"
-            tokenSplit reader (newValue::acc) reader.LineNumber (reader.LinePosition + 1)
-        | 1, _ ->
-            let newValue = (reader.Value |> string)
-            tokenSplit reader (newValue::acc) reader.LineNumber (reader.LinePosition + 1)
-        | _, _ ->
-            tokenSplit reader acc startLine startIdx
-    tokenSplit reader [] 0 0
-    |> List.rev
-    |> Seq.iter Console.WriteLine
+let main args =
+    match args with
+    | [|"client"|] ->
+        let client = new NSchema.Model.Invoicing.InvoiceService("amqp://localhost", "testnschema")
+        let userInfo = client.Authenticate "manager" "password"
+        userInfo.Name |> System.Console.WriteLine
+        userInfo.Status |> System.Console.WriteLine
+        let userInfo = client.Authenticate "manager" "password"
+        userInfo.Name |> System.Console.WriteLine
+        userInfo.Status |> System.Console.WriteLine
+        let invoiceList = client.GetInvoiceList "manager" "password"
+        invoiceList |> List.length |> System.Console.WriteLine
+        let invList, err = client.GetTwoValueOperation "manager" "password"
+        invList |> List.length |> System.Console.WriteLine
+        let invList, err = client.GetTwoValueOperation "manager2" "password"
+        match err with
+        | null ->
+            ()
+        | _ ->
+            err |> System.Console.WriteLine
+    | _ ->
+        (new NSchema.Model.Invoicing.InvoiceServiceSrv("amqp://localhost", "testnschema"))._Listen() |> ignore
     0

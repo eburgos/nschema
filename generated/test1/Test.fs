178e6867239e1ca47958193ab126ea3f9cd64736
Eduardo Burgos <eburgos@gmail.com>
2014-12-22 16:18:49 -0400

    amqpRpc working with F#

--- generated/test1/Test.fs
+++ generated/test1/Test.fs
@@ -6,25 +6,24 @@ open Newtonsoft.Json.Converters
 
 [<EntryPoint>]
 let main args =
-    match args with
-    | [|"client"|] ->
-        let client = new NSchema.Model.Invoicing.InvoiceService("amqp://localhost", "testnschema")
-        let userInfo = client.Authenticate "manager" "password"
-        userInfo.Name |> System.Console.WriteLine
-        userInfo.Status |> System.Console.WriteLine
-        let userInfo = client.Authenticate "manager" "password"
-        userInfo.Name |> System.Console.WriteLine
-        userInfo.Status |> System.Console.WriteLine
-        let invoiceList = client.GetInvoiceList "manager" "password"
-        invoiceList |> List.length |> System.Console.WriteLine
-        let invList, err = client.GetTwoValueOperation "manager" "password"
-        invList |> List.length |> System.Console.WriteLine
-        let invList, err = client.GetTwoValueOperation "manager2" "password"
-        match err with
-        | null ->
-            ()
-        | _ ->
-            err |> System.Console.WriteLine
-    | _ ->
+    async {
         (new NSchema.Model.Invoicing.InvoiceServiceSrv("amqp://localhost", "testnschema"))._Listen() |> ignore
+    } |> Async.Start
+    let client = new NSchema.Model.Invoicing.InvoiceService("amqp://localhost", "testnschema")
+    let userInfo = client.Authenticate "manager" "password"
+    userInfo.Name |> System.Console.WriteLine
+    userInfo.Status |> System.Console.WriteLine
+    let userInfo = client.Authenticate "manager" "password"
+    userInfo.Name |> System.Console.WriteLine
+    userInfo.Status |> System.Console.WriteLine
+    let invoiceList = client.GetInvoiceList "manager" "password"
+    invoiceList |> List.length |> System.Console.WriteLine
+    let invList, err = client.GetTwoValueOperation "manager" "password"
+    invList |> List.length |> System.Console.WriteLine
+    let invList, err = client.GetTwoValueOperation "manager2" "password"
+    match err with
+    | null ->
+        ()
+    | _ ->
+        err |> System.Console.WriteLine
     0
---
+++ lib/provider/target/fsharp/README.md
@@ -0,0 +1,10 @@
+Targetting F# with NSchema
+==========================
+
+Binds available:
+
+* object
+    Generates F# classes.
+
+* amqpRpc
+    Generates RabbitMQ message producers and consumers for a RPC endpoint
